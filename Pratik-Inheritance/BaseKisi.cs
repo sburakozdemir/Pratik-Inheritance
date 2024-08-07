46a8403787e2eaceb817920bbaa3a7a4a668e77b
sburakozdemir <sburakozdemir00@gmail.com>
2024-08-07 16:56:17 +0300

    Add project files.

---
+++ Pratik-Inheritance/BaseKisi.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace Pratik_Inheritance
+{
+    // BaseKisi sınıfı, diğer sınıflar için temel oluşturur
+    public abstract class BaseKisi
+    {
+        // Temel özellikler
+        public string Ad { get; set; }
+        public string Soyad { get; set; }
+
+        // Kişiyi tanıtan virtual metot
+        public virtual void Tanıt()
+        {
+          
+            Console.WriteLine($"Adı: {Ad}\nSoyadı: {Soyad}");
+        }
+    }
+
+    // Ogrenci sınıfı, BaseKisi'den miras alır
+    public class Ogrenci : BaseKisi
+    {
+        // Öğrenciye özel özellik
+        public int OgrenciNo { get; set; }
+
+        // Öğrenciyi tanıtan override edilmiş metot
+        public override void Tanıt()
+        {
+            Console.WriteLine($"Öğrencinin;\nAdı: {Ad}\nSoyadı: {Soyad}\nÖğrenci No'su: {OgrenciNo}");
+        }
+
+        // Yapıcı metot
+        public Ogrenci(string ad, string soyad, int ogrenciNo)
+        {
+            Ad = ad;
+            Soyad = soyad;
+            OgrenciNo = ogrenciNo;
+        }
+    }
+
+    // Ogretmen sınıfı, BaseKisi'den miras alır
+    public class Ogretmen : BaseKisi
+    {
+        // Öğretmene özel özellik
+        public int MaasBilgisi { get; set; }
+
+        // Öğretmeni tanıtan override edilmiş metot
+        public override void Tanıt()
+        {
+            Console.WriteLine($"Öğretmenin;\nAdı: {Ad}\nSoyadı: {Soyad}\nMaaş Bilgisi: {MaasBilgisi} TL");
+        }
+
+        // Yapıcı metot
+        public Ogretmen(string ad, string soyad, int maasBilgisi)
+        {
+            Ad = ad;
+            Soyad = soyad;
+            MaasBilgisi = maasBilgisi;
+        }
+    }
+}
---
+++ Pratik-Inheritance/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Pratik_Inheritance;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Örnek bir öğretmen nesnesi oluştur ve bilgilerini yazdır
+        Ogretmen ogretmen1 = new Ogretmen("Ali", "Dursun", 25000);
+        ogretmen1.Tanıt();
+
+        Console.WriteLine(); // Boş satır ekle
+
+        // Örnek bir öğrenci nesnesi oluştur ve bilgilerini yazdır
+        Ogrenci ogrenci1 = new Ogrenci("Berk", "Bayındır", 198001);
+        ogrenci1.Tanıt();
+    }
+}
